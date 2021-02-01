4f27c47c9a5b6cb46065f82d8dae7213f1f481fc
Mohammed Alfathi <iizekalfthi@gmail.com>
2021-02-01 19:11:08 +0400

    modified  app.py file again

--- app.py
+++ app.py
@@ -1,14 +1,13 @@
-class Input_anime():   
-    def __init__(self, name, all=False):   
+class Input_anime():
+    def __init__(self, name, all=False):
         self.name = str(name)
-        if self.name == "all":   
+        if self.name == "all":
             return self.name
         self.all = all
-        if self.all:    
+        if self.all:
             return self.name
-    def git_animeList(self):   
+    def git_animeList(self):
         print(Input_anime(name="all", all=True))
-    def add_anime(self, name):   
+    def add_anime(self, name):
         name = input("Enter  the anime name: ") 
         Input_anime(name=name)
-        
